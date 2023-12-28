bbdbcb866251001e2a82a8689840d716850177c6
i.iliev <i.t.iliev@armf.bg>
2023-12-29 01:25:29 +0200

    added start/stop. Pipeline structure implemented

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import psutil
+import json
 import tkinter as tk
 import tkinter.scrolledtext as scrolledtext
 import socket
 import struct
 import threading
+from datetime import datetime as dt
 
 from protocol_strategies import (
     PROTOCOLS
@@ -27,6 +29,8 @@ class PacketSniffer:
         start_button = tk.Button(root, text="Start Sniffing", command=self.start_sniffing)
         start_button.pack()
 
+        self.capturing = False
+
     def start_sniffing(self):
         # Get the filter IP from the entry widget
         self.filter_ip = self.filter_entry.get()
@@ -35,20 +39,41 @@ class PacketSniffer:
         sniffing_thread.start()
 
     def sniff_packets(self):
+        self.capturing = not self.capturing
         raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
         raw_socket.bind(("192.168.0.234", 0))  # Replace with your IP address
         raw_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
         raw_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
+        now = dt.now().strftime('%Y-%m-%d %H-%M-%S')
         try:
             while True:
-                data, addr = raw_socket.recvfrom(65536)
-                self.parse_packet(data)
+
+                try:
+                    with open(f'capture-{now}.json', 'r') as f:
+                        d = json.load(f)
+                except FileNotFoundError:
+                    d = []
+                except json.decoder.JSONDecodeError:
+                    d = []
+
+                if self.capturing: # inappropriate place for this TODO
+                    data, addr = raw_socket.recvfrom(65536)
+                    decoded_data = self.parse_packet(data)
+                    d.append(decoded_data)
+
+                    with open(f"capture-{now}.json", "w") as f:
+                        json.dump(d, f, indent=4)
+
+                    self.apply_filters(decoded_data)
+
+
         except KeyboardInterrupt:
             print("\nStopping the packet sniffer.")
         finally:
             raw_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
             raw_socket.close()
+        f.seek(0, 2)
+        json.dump(d, f, indent=4)
 
     def parse_packet(self, data):
         ip_header = struct.unpack("!BBHHHBBH4s4s", data[:20])
@@ -63,11 +88,29 @@ class PacketSniffer:
         else:
             protocol_name = protocol.name
             src_port, dst_port, process_name = protocol.decode(data)
+        return {
+            "src_ip": src_ip,
+            "src_port": src_port,
+            "dst_ip": dst_ip,
+            "dst_port": dst_port,
+            "protocol_name": protocol_name,
+            "process_name": process_name,
+            "raw_data": data.decode("latin-1"),
+        }
+
+    def apply_filters(self, data):
+        src_ip = data["src_ip"]
+        src_port = data["src_port"]
+        dst_ip = data["dst_ip"]
+        dst_port = data["dst_port"]
+        protocol_name = data["protocol_name"]
+        process_name = data["process_name"]
+        raw_data = data["raw_data"]
 
         info = f"Src: {src_ip} (Port: {src_port}) -> Dst: {dst_ip} (Port: {dst_port}) | Protocol: {protocol_name} | Process: {process_name}\n"
 
         # Display the raw data (hexadecimal representation) in the GUI
-        raw_data = " ".join(f"{byte:02X}" for byte in data)
+        raw_data = " ".join(f"{byte:02X}" for byte in raw_data.encode("latin-1"))
         info += f"Raw Data: {raw_data}\n\n"
 
         # if process_name != "N/A":
